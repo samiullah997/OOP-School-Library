64854d2806cc21e8b199ac018091b813b5288efe
samiullah997 <samiullahk997@gmail.com>
2022-11-01 14:42:41 +0500

    solve linter errors

--- nameable.rb
+++ nameable.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Nameable < Person
-    def correct_name
-        raise NotImplementedError, 'correct_name has to be defined'
-    end
+  def correct_name
+    raise NotImplementedError, 'correct_name has to be defined'
+  end
 end
